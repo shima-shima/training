5f7bef890a22d4873aa23c738a18e2f765165db5
shima-shima <19295364+shima-shim@users.noreply.github.com>
2023-11-19 10:36:19 +0000

    feat(ruby/todo): stoer作成
    とりあえずストレージはインメモリ実装

---
+++ ruby_todo_api/stoer.rb
@@ -0,0 +1,26 @@
+require_relative './todo'
+require 'securerandom'
+
+class TodoFactory
+    def new(title, body)
+        Todo.new(SecureRandom.uuid, title, body)
+    end
+end
+
+class InMemoryTodoRepository
+    def initialize
+        @todos = {}
+    end
+
+    def find_all
+        @todos
+    end
+
+    def find(id)
+        @todos[id]
+    end
+
+    def save(todo)
+        @todos[todo.id] = todo
+    end
+end
