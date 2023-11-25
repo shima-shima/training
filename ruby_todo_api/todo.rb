919b3c3c933bf25581abe43018933a2fb745a315
shima-shima <19295364+shima-shim@users.noreply.github.com>
2023-11-25 14:08:31 +0000

    fix(ruby/todo): statusをopenに名前変更

--- ruby_todo_api/todo.rb
+++ ruby_todo_api/todo.rb
@@ -1,10 +1,23 @@
 class Todo
-    attr_reader :id
-    attr_accessor :status, :title, :body
+    attr_reader :id, :open
+    attr_accessor :title, :body
 
     def initialize(id, title, body)
         @id = id
         @title = title
         @body = body
+        open
+    end
+
+    def open
+        @open = true
+    end
+
+    def close
+        @open = false
+    end
+
+    def open?
+        @open
     end
 end
